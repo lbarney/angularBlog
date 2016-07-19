ef947248c3f4734229c1179df7325155a5ad7a03
Lance Barney <lbarney@littleappletech.com>
2016-07-19 14:36:56 -0600

    adding initial file structure

---
+++ public/mainCtrl.js
@@ -0,0 +1,3 @@
+angular.module('blogApp').controller('mainCtrl', function($scope, mainServ){
+	$scope.test = 'this is a test';
+});
---
+++ public/mainServ.js
@@ -0,0 +1,3 @@
+angular.module('blogApp').service('mainServ', function($http){
+
+});
